b547d19285442e8ae21c880a7c9454529208e75d
jaromhulet <jarom.hulet@gmail.com>
2020-01-12 17:45:54 -0600

    added code that will become random walk to hill climb.  Still needs to be adjusted to be correct code.

--- hill_climb.py
+++ hill_climb.py
@@ -133,6 +133,51 @@ class hillClimb(nbr.nbrHood):
             #bestDist = globalBestDist
             
             print("done")
+            
+            #conditionally execute hill climb with best accept
+        elif selectMethod == 'RANDOM WALK':
+            
+            globalBestNbr = startSolution
+            globalBestDist = self.totalDist(self.groupMetrics(startSolution,aggMethod),distMetric)
+                
+            for j in range(0,restarts):
+                    
+                
+                currentSolution = startSolution
+                
+                done = 0
+                
+                while done == 0:
+                        
+                    bestNbr = currentSolution
+                    bestDist = self.totalDist(self.groupMetrics(currentSolution,aggMethod),distMetric)
+                        
+                    for i in self.createNbrhood(currentSolution,self.numGroups):
+                        if self.totalDist(self.groupMetrics(i,aggMethod),distMetric) < self.totalDist(self.groupMetrics(bestNbr,aggMethod),distMetric):
+                            bestNbr = i
+                            bestDist = self.totalDist(self.groupMetrics(i,aggMethod),distMetric)
+                            print(bestDist)
+                                
+                    if currentSolution == bestNbr:
+                        done = 1
+                    else:
+                        currentSolution = bestNbr 
+                            
+                            
+                print("restart")
+                    
+                if bestDist < globalBestDist:
+                    globalBestNbr = bestNbr
+                    globalBestDist = self.totalDist(self.groupMetrics(bestNbr,aggMethod),distMetric)
+                    print("New Global Best = %s" % globalBestDist)
+                        
+                        
+                #put the globals back into the locals so the return statement returns the global bests
+                #bestNbr = globalBestNbr
+                #bestDist = globalBestDist
+                
+                print("done")        
+            
                     
                 
             
