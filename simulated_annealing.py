73931903b893e2cd59dfc9312d4eb21907d90878
jaromhulet <jarom.hulet@gmail.com>
2020-02-02 16:22:18 -0600

    corrected multiple issues with the simulated annealing algorithm code'

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -21,50 +21,39 @@ class simulatedAnnealing(nbr.nbrHood):
         #create a starting solution
         startSolution = self.startSol(randStart=randStart,startMetric=startMetric)        
         
-        globalBestNbr = startSolution
-        globalBestDist = self.totalDist(self.groupMetrics(startSolution,aggMethod),distMetric)
-        
         currentSolution = startSolution
-        
-        
-        #loop through restart
-        for f in range(0,restarts):
+        currentDist = self.totalDist(self.groupMetrics(startSolution,aggMethod),distMetric)
             
-            for temperature in coolingSchedule:
+        for temperature in coolingSchedule:
                 
-                for iters in range(0,tempIters):
+            print('temparature is %s' % temperature)
                 
-                    bestNbr = currentSolution
-                    bestDist = self.totalDist(self.groupMetrics(currentSolution,aggMethod),distMetric)
+            for iters in range(0,tempIters):
+                              
+                temp_nbr = self.createNbrhood(currentSolution,2)
+                
+                        
+                obs = obs + 1
+                       
+                        
+                #calculate difference between current nbr and the best nbr
+                delta = self.totalDist(self.groupMetrics(temp_nbr[0],aggMethod),distMetric) - self.totalDist(self.groupMetrics(currentSolution,aggMethod),distMetric)
                     
-                    for i in self.createNbrhood(currentSolution,self.nbrhoodsize):
+                #calculate acceptance probability
+                if delta < 0:
+                    acceptProb = 1.1
+                else:
+                    acceptProb = exp(-delta/temperature)
                         
-                        obs = obs + 1
-                        
-                        #calculate difference between current nbr and the best nbr
-                        delta = self.totalDist(self.groupMetrics(i,aggMethod),distMetric) - self.totalDist(self.groupMetrics(bestNbr,aggMethod),distMetric)
-                        
-                        #calculate acceptance probability
-                        acceptProb = exp(delta/temperature)
-                        
-                        #decide if it will be accepted 
-                        if acceptProb > random.uniform(0,1):
-                            bestNbr = i
-                            bestDist = self.totalDist(self.groupMetrics(i,aggMethod),distMetric)
-                            print(bestDist)
-                            #add break to make it first accept
-                            break
-                        
-                print("restart")
-                
-                if bestDist < globalBestDist:
-                    globalBestNbr = bestNbr
-                    globalBestDist = self.totalDist(self.groupMetrics(bestNbr,aggMethod),distMetric)
-                    print("New Global Best = %s" % globalBestDist)
+                #decide if it will be accepted 
+                if acceptProb > random.uniform(0,1):
+                    currentSolution = temp_nbr[0]
+                    currentDist = self.totalDist(self.groupMetrics(currentSolution,aggMethod),distMetric)
+                    print(currentDist)
                     
                     
             obs_statement = ("%s Solutions Examined" % obs)
             print("done")              
             
         #return solution metric and solution
-        return [self.groupMetrics(globalBestNbr,aggMethod),globalBestNbr,globalBestDist,obs_statement]                  
+        return [self.groupMetrics(currentSolution,aggMethod),currentSolution,currentDist,obs_statement]                  
